92eaae19072f66a4278d8fd0abe980f04663b598
macfrei <lene@7freiheiten.de>
2020-10-12 14:41:54 +0200

    Add theme as json for colors, add link to slide

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
 } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import FirstSlide from './weekOne/FirstSlide'
 import SecondSlide from './weekTwo/SecondSlide'
 
 function App() {
+  
   return (
     <Router>
-      <Navigation />
-
       <Switch>
         <Route path="/weekOne/FirstSlide">
             <FirstSlide />
         </Route>
         <Route path="/weekTwo/SecondSlide">
             <SecondSlide />
+        </Route>
+        <Route path="/">
+          <Navigation />
         </Route>
       </Switch>
     </Router>
--- src/weekOne/FirstSlide.js
+++ src/weekOne/FirstSlide.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Deck, Slide, Heading } from 'spectacle';
-
-const theme = {
-  colors: {
-    primary: "yellow",
-    scondary: "green" ,
-  }
-}
+import theme from '../components/theme.json'
+import {Link} from 'react-router-dom'
 
 export default function FirstSlide() {
     return (
         <Deck theme={theme}>
-          <Slide backgroundColor="primary">
+          <Slide backgroundColor="nfblue">
             <Heading>Welcome to Week One!</Heading>
+            <Link to="/">Link</Link>
           </Slide>
-          <Slide backgroundColor="scondary">
+          <Slide backgroundColor="nforange">
             <Heading>Welcome to Spectacle</Heading>
           </Slide>
         </Deck>
